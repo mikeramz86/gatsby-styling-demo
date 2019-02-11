015a323818c22322e03f8137e885e6c549a7ea37
Mike <mikeramzdesign@gmail.com>
2019-02-10 21:42:25 -0800

    init commit up to specific area

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,13 @@
+import React from "react"
+// import styles from "./about-css-modules.module.css"
+import Container from "../components/container"
+
+// console.log(styles)
+
+export default () => (
+    <Container>
+        <h1>About CSS Modules</h1>
+        <p>CSS Modules are cool</p>
+    </Container>
+)
+
